e2b0a1bc21f5ccb50a913fd1cef5bed0593b662c
Nolan Frame <frame.nolan@gmail.com>
2022-04-02 20:57:14 -0700

    pseudo code and beginning of html

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,36 @@
 console.log("hello")
 
+// View Highscores updates page to show leaderboard
+
+// Interval countdown timer to show time remaining
+//     Timer starts counting when Start Game button is pushed
+//     Update to subtract time when question answered incorrectly
+//     When timer hits 0, game ends
+
+// When Start Button is clicked...
+//     Countdown timer counts down
+//     First question is shown
+//         Four buttons with possible answers are shown
+//         On answer button click...
+//             when correct: show Right Message, update page with new question/answers, add 1 to score
+//             when incorrect: show Wrong message, update page with new question/answers, subtract time from score
+//     When timer ends, change to Final Score Page
+//     When all questions are answered, change to final score page
+
+// Final Score Page:
+//     display score count
+//     show form for leaderboard input, submit button
+//     Save input to localStorage
+    
+// Leaderboard Page:
+//     show localStorage of leaderboard
+//     concat sumbitted name and score
+//     ranked by score, highest to lowest
+//     button to clear localStorage of leaderboard
+//     play again returns to Start Game button, or just runs the start game function
+    
+// Array of questions/answers
+//     ...could be two arrays (one for questions, sub array(s) for answers)
+//     ...may need a var for index of correct answer
+    
+    
