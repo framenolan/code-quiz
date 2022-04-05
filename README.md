34fddaa130212830cf90f46cdda6ca2ae7805a2f
Nolan Frame <frame.nolan@gmail.com>
2022-04-04 19:11:05 -0700

    completed

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # code-quiz
+
+Deployed App: https://framenolan.github.io/code-quiz
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## My Approach
+
+I divided each individual process into a function and used event listeners to call those functions. This resulted in a lot of variables, and the code could probably by dryer.
+
+When the player starts the game, a countdown timer from 30 seconds begins counting down. If a player guesses correctly, their score increases by 1 point. If a player guesses incorrectly, 5 seconds are removed from the timer. Whenever a player guesses an answer, the next question and answer set are shown. When the timer reaches 0, the game is over. When all questions are answered, the remaining time is added to the player's score for a final score.
+
+When shown the final score, the player is able to save their name to a leaderboard. The leaderboard page can be access from a button in the header at any time.
+
+## Troubles and Issues
+
+I was not able to figure out how keep the leaderboard between sessions, but this was not a technical requirement.
+
+I was not able to figure out how to rank the leaderboard by highest score to lowest score, but this was not a technical requirement.
+
+In my original approach, I attempted to randomly generate simple arithmetic questions and verify the answer, but it was getting too complicated and not in the technical requirements, so I pivoted to queer history questions in arrays instead.
--- assets/script.js
+++ assets/script.js
@@ -1,233 +1,193 @@
+// querySelectors for navigation buttons
 var startGame = document.querySelector(".startButton");
 var showHighscore = document.querySelector(".showLeaderboard")
 var viewHighscore = document.querySelector(".viewLeaderboard")
 var playAgain = document.querySelector(".playAgain")
 var startAgain = document.querySelector(".startAgain")
+var saveScore = document.querySelector(".saveScore")
+var clearLeaderboard = document.querySelector(".clearLeaderboard")
 
+// querySelectors for question text and answer buttons
 var answerButton1 = document.querySelector("#answer1")
 var answerButton2 = document.querySelector("#answer2")
 var answerButton3 = document.querySelector("#answer3")
 var answerButton4 = document.querySelector("#answer4")
 var questionText = document.querySelector(".questionText")
+var result = document.querySelector(".result")
 
+// querySelectors for the four "pages"
 var instructionsPage = document.querySelector(".instructionsPage");
 var questionPage = document.querySelector(".questionPage");
 var leaderboardPage = document.querySelector(".leaderboardPage");
 var scorePage = document.querySelector(".scorePage");
 
+// Variables for time remaining
 var secondsLeft = 30;
 var timeEl = document.querySelector("#countdownTimer");
 
-var score = localStorage.getItem("score");
+// Variable for score
+var score = 0;
+var timerInterval = ''
 
-var question1 = ["What year did the Stonewall Riots happen?", "1972", "1965", "1969", "1975", "1965"]
+// Arrays of questions to be asked, with question text as i=0, answer choices at i=1-4, correct answer at i=5
+var question1 = ["In what year did the Stonewall Riots happen?", "1972", "1965", "1969", "1975", "1969"]
 var question2 = ["Who was the first winner of RuPaul's Drag Race?", "Bebe Zahara Benet", "Tyra Sanchez", "Ongina", "Lady Bunny", "Bebe Zahara Benet"]
-var questionList = [question1, question2]
+var question3 = ["Who wrote Hedwig and the Angry Inch?", "Harvey Milk", "Elton John", "Cyndy Lauper", "John Cameron Mitchell", "John Cameron Mitchell"]
+var question4 = ["What TV show featured the largest transgender cast in History?", "Transparent", "Pose", "Will & Grace", "Glee", "Pose"]
+var question5 = ["In what year did same sex marriage become legal at the federal level in the United States?", "1969", "2001", "2012", "2015", "2015"]
+var questionList = [question1, question2, question3, question4, question5]
 
-var currentQuestion = question1[0]
-var currentQuestionArray = question1
-var correctAnswer = currentQuestion[6]
+// Variables for tracking the current question and correct answer
+var currentQuestion = question1
+var correctAnswer = currentQuestion[5]
+var currentQuestionIndex = 0
 
-console.log(currentQuestion)
-console.log(currentQuestionArray)
+// Event Listeners for when an answer is guessed
+answerButton1.addEventListener("click",recordGuess)
+answerButton2.addEventListener("click",recordGuess)
+answerButton3.addEventListener("click",recordGuess)
+answerButton4.addEventListener("click",recordGuess)
 
-
-// flashcards()
-
+// Starts the game
 function start() {
     startTime()
+    changeQuestion()
     instructionsPage.setAttribute("data-show", "hidden")
     questionPage.setAttribute("data-show", "show")
-    questionText.textContent = question1[0]
-    answerButton1.value = question1[1]
-    answerButton2.value = question1[2]
-    answerButton3.value = question1[3]
-    answerButton4.value = question1[4]
-    
-    var guess = document.querySelector(".questionPage")
-    var guessValue = guess.value
-    answerButton1.addEventListener("click",answerQuestion)
-    answerButton2.addEventListener("click",answerQuestion)
-    answerButton3.addEventListener("click",answerQuestion)
-    answerButton4.addEventListener("click",answerQuestion)
-    
+}
+
+// Saves guess value to be compared
+function recordGuess (event) {
+    var currentGuess = event.target
+    var guessValue = currentGuess.value
     answerQuestion(guessValue)
-    
-    console.log(currentQuestion)
-    console.log(currentQuestionArray)
-    newQuestion(questionList.indexOf(currentQuestionArray))
-    console.log(questionList.indexOf(currentQuestionArray))
-    console.log(currentQuestion)
-    console.log(currentQuestionArray)
+    return
 }
 
+// Compares player guess to correct answer
+// If matched, score increases by 1
+// If not matched, 5 seconds removed from timer
+// Either way, then the question moves to the next one
+function answerQuestion(guessValue) {
+    if (guessValue == correctAnswer) {
+        score++;
+        // localStorage.setItem("score", score);
+        result.textContent = "Correct!";
+        newQuestion(currentQuestionIndex);
+    } else {
+        result.textContent = "Wrong :(";
+        reduceTime();
+        newQuestion(currentQuestionIndex);
+    }
+}
+
+// Subtracts 5 seconds from timer, used if a question is answer incorrectly
+function reduceTime() {
+    secondsLeft = secondsLeft-5
+}
+
+// Changes current question to next question in list, game ends if player runs out of questions
 function newQuestion(i) {
-    currentQuestion = questionList[i+1];
-    console.log(currentQuestion)
-    console.log(currentQuestionArray)
-    // currentQuestion = currentQuestionArray[0];
-    console.log(currentQuestion)
-    console.log(currentQuestionArray)
+    if (currentQuestionIndex >= questionList.length-1) {
+        gameOver();
+        return;
+    } else {
+        currentQuestion = questionList[i+1];
+        currentQuestionIndex = currentQuestionIndex+1;
+        correctAnswer = currentQuestion[5];
+        changeQuestion()
+    }
 }
 
-// When time runs out, page changes to scorePage
+// Changes question and answer text to current question
+function changeQuestion(){
+    questionText.textContent = currentQuestion[0]
+    answerButton1.value = currentQuestion[1]
+    answerButton2.value = currentQuestion[2]
+    answerButton3.value = currentQuestion[3]
+    answerButton4.value = currentQuestion[4]
+}
+
+// When time runs out, navigates to scorePage, adds any remaining seconds to score total
 function gameOver() {
-    secondsLeft = 5
+    clearInterval(timerInterval)
+    timeEl.textContent = "--"
+    score = secondsLeft + score
     questionPage.setAttribute("data-show", "hidden")
     scorePage.setAttribute("data-show", "show")
     var yourScore = document.querySelector("#yourScore")
     yourScore.textContent = score
 }
 
+// Navigates to instructionPage from scorePage or leaderboardPage, resets starter variables
 function reset() {
     scorePage.setAttribute("data-show", "hidden")
     leaderboardPage.setAttribute("data-show", "hidden")
     instructionsPage.setAttribute("data-show", "show")
+    score = 0;
+    secondsLeft = 30;
+    currentQuestion = question1;
+    correctAnswer = currentQuestion[5];
+    currentQuestionIndex = 0;
+    result.textContent = "";
+    clearInterval(timerInterval)
 }
 
+// Navigates to leaderboardPage
 function highscore() {
     scorePage.setAttribute("data-show", "hidden")
     instructionsPage.setAttribute("data-show", "hidden")
+    questionPage.setAttribute("data-show", "hidden")
     leaderboardPage.setAttribute("data-show", "show")
 }
 
 // Interval countdown timer to show time remaining
 function startTime() {
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft
-        
         if(secondsLeft <= 0) {
             clearInterval(timerInterval);
             gameOver();
+            return
         }
     }, 1000)
     return secondsLeft;
 }
 
-function answerQuestion(guessCheck) {
-    if (guessCheck == correctAnswer) {
-        score++;
-        localStorage.setItem("score", score);
-        newQuestion();
-    } else {
-        secondsLeft-5;
-        newQuestion();
-    }
-    // Update .questionText textContent to a question
 
-}
-
-// function flashcards() {
-//     var arrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//     var arrayOperators = ["-","+","*"]
-    
-//     function pullChar(array) {
-//         varRandom = Math.floor(Math.random() * array.length)
-//         currentChar = array[varRandom]
-//         return(currentChar)
-//     }
-    
-//     function doTheWrongMath() {
-//         valueA = pullChar(arrayNum)
-//         operator = pullChar(arrayOperators)
-//         valueB = pullChar(arrayNum)
-
-//         console.log(valueA,operator,valueB)
-        
-//         answer = eval(valueA + operator + valueB)
-//         console.log(answer)
-//         return(answer)
-//     }
-
-//     // create array of possible answers
-//     var possibleArray = [0]
-//     for (let i = 0; i <= arrayNum.length; i++) {
-//         var result = doTheWrongMath()
-//         if (possibleArray.includes(result)) {
-//         doTheWrongMath()
-//         } else {
-//         possibleArray.push(result)
-//         console.log(possibleArray)
-//         }
-//     }
-
-//     var wrongAnswer1 = pullChar(possibleArray)
-//     var wrongAnswer2 = pullChar(possibleArray)
-//     var wrongAnswer3 = pullChar(possibleArray)
-
-//     // if (wrongAnswer1 !== wrongAnswer2) {
-//     //     wrongAnswer2 = pullChar(possibleArray)
-//     // } else {
-//     //     return wrongAnswer3
-//     // }
-
-//     var wrongEl1 = document.querySelector("#answer2")
-//     var wrongEl2 = document.querySelector("#answer3")
-//     var wrongEl3 = document.querySelector("#answer4")
-
-//     wrongEl1.textContent = wrongAnswer1
-//     wrongEl2.textContent = wrongAnswer2
-//     wrongEl3.textContent = wrongAnswer3
-
-//     // chose 3 other answers out of possible array
-
-//     // verify that chosen answers don't match real answer
-
-//     // update text content of answer buttons
-// }
-
-//CREATE ARRAY LIST OF QUESTIONS
-    //1)EACH QUESTION IS ITS OWN ARRAY
-        //ARRAY[0] IS THE QUESTION
-        //ARRAY[1-6] ARE THE CHOICES
-        //ARRAY[6] IS THE CORRECT ANSWER
-
-
-
-
+// Button to view Leaderboard at any time
 viewHighscore.addEventListener("click", highscore)
+
 // Button to start the game
 startGame.addEventListener("click", start);
 
 // On Score page, player chooses to play again or view the leaderboard
 playAgain.addEventListener("click", reset)
-showHighscore.addEventListener("click", highscore)
+saveScore.addEventListener("click", onTheBoard)
 
 // Leaderboard Page buttons
 startAgain.addEventListener("click", reset)
+clearLeaderboard.addEventListener("click", clearTheBoard)
 
-// ...maybe setup a data attribute of right/wrong
+// Saves current score and name to leaderboard
+function onTheBoard() {
+    var scoreName = document.querySelector("#scoreName")
+    var playerName = scoreName.value
+    var playerScore = score
+    localStorage.setItem("playerName", playerName);
+    localStorage.setItem("playerScore", playerScore);
+    highscore();
 
+    var setRankings = document.querySelector(".rankings")
 
-//     Timer starts counting when Start Game button is pushed
-//     TODO: Update to subtract time when question answered incorrectly
-//     When timer hits 0, game ends
+    var newRanking = document.createElement("li");
+    newRanking.textContent = playerName+" scored "+playerScore+" points";
+    setRankings.appendChild(newRanking);
+}
 
-// When Start Button is clicked...
-//     Countdown timer counts down
-//     First question is shown
-//         Four buttons with possible answers are shown
-//         On answer button click...
-//             when correct: show Right Message, update page with new question/answers, add 1 to score
-//             when incorrect: show Wrong message, update page with new question/answers, subtract time from score
-//     When timer ends, change to Final Score Page
-//     When all questions are answered, change to final score page
-
-// Final Score Page:
-//     display score count
-//     show form for leaderboard input, submit button
-//     Save input to localStorage
-    
-// Leaderboard Page:
-//     show localStorage of leaderboard
-//     concat sumbitted name and score
-//     ranked by score, highest to lowest
-//     button to clear localStorage of leaderboard
-//     play again returns to Start Game button, or just runs the start game function
-    
-// Array of questions/answers
-//     ...could be two arrays (one for questions, sub array(s) for answers)
-//     ...may need a var for index of correct answer
-    
-    
+// Deletes oldest score; couldn't figure out how to get it to delete all scores at once so it starts with first li
+function clearTheBoard() {
+    var setRankings = document.querySelector(".rankings li")
+    setRankings.remove("li")
+}
